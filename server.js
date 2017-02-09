53c97328d255b5a734331681ba58fe88b1906dd7
jeremym <jeremym@balink.net>
2017-02-09 14:32:34 +0200

    test come back

--- server.js
+++ server.js
@@ -3,60 +3,78 @@ var bodyParser = require('body-parser')
 var app =express();
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended: true}));
-// var chilkat = require('chilkat_node7_win32'); 
+
+var chilkat = require('chilkat_node7_win32'); 
+
+// var os = require('os');
+// if (os.platform() == 'win32') {  
+//     var chilkat = require('chilkat_node7_win32'); 
+// } 
+// else if (os.platform() == 'linux') {
+//     if (os.arch() == 'arm') {
+//         var chilkat = require('chilkat_node6_arm');
+//     } else if (os.arch() == 'x86') {
+//         var chilkat = require('chilkat_node6_linux32');
+//     } else {
+//         var chilkat = require('chilkat_node6_linux64');
+//     }
+// } else if (os.platform() == 'darwin') {
+//     var chilkat = require('chilkat_node6_macosx');
+// }
+
 var test;
 var port =process.env.PORT || 8080;
 
-// function chilkatExample(csv) {
+function chilkatExample(csv) {
 
-//     var zip = new chilkat.Zip();
+    var zip = new chilkat.Zip();
     
 
-//     var success;
-//      //  Any string unlocks the component for the 1st 30-days.
-//     success = zip.UnlockComponent("Anything for 30-day trial");
-//     if (success !== true) {
-//         console.log(zip.LastErrorText);
-//         return;
-//     }
+    var success;
+     //  Any string unlocks the component for the 1st 30-days.
+    success = zip.UnlockComponent("Anything for 30-day trial");
+    if (success !== true) {
+        console.log(zip.LastErrorText);
+        return;
+    }
 
 
-//     success = zip.NewZip("test2.zip");
-//     if (success !== true) {
-//         console.log(zip.LastErrorText);
-//         return;
-//     }
+    success = zip.NewZip("test2.zip");
+    if (success !== true) {
+        console.log(zip.LastErrorText);
+        return;
+    }
 
-//     zip.SetPassword("secret");
-//     zip.PasswordProtect = true;
+    zip.SetPassword("secret");
+    zip.PasswordProtect = true;
 
-//     // var saveExtraPath;
-//     // saveExtraPath = false;
-//     // success = zip.AppendOneFileOrDir("./index.html",saveExtraPath);
-//     // if (success !== true) {
-//     //     console.log(zip.LastErrorText);
-//     //     return;
+    // var saveExtraPath;
+    // saveExtraPath = false;
+    // success = zip.AppendOneFileOrDir("./index.html",saveExtraPath);
+    // if (success !== true) {
+    //     console.log(zip.LastErrorText);
+    //     return;
 
-//     // }
+    // }
 
-//     var entry = zip.AppendString2("HelloWorld2.csv",csv,"utf-8");
+    var entry = zip.AppendString2("HelloWorld2.csv",csv,"utf-8");
 
-//     var success = zip.WriteZipAndClose();
-//     if (success !== true) {
-//         console.log(zip.LastErrorText);
-//         return;
-//     }
-//     test = JSON.stringify(zip);
+    var success = zip.WriteZipAndClose();
+    if (success !== true) {
+        console.log(zip.LastErrorText);
+        return;
+    }
+    test = JSON.stringify(zip);
 
-//     console.log("Zip Created!");
+    console.log("Zip Created!");
 
-// }
+}
 
 
 app.post('/createZip',function(req, res){
     var body = req.body;
     res.set('Content-Type', 'text/plain');
-	// chilkatExample(req.body);
+	chilkatExample(req.body);
 	res.send(`You sent: ${body} to Express`);
 });
 
