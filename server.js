be8b7abf0a2746d290778a14e5f4a7fad89956f9
jeremym <jeremym@balink.net>
2017-02-13 16:58:37 +0200

    no message

--- server.js
+++ server.js
@@ -114,13 +114,13 @@ function connectToSF(callback2){
 	   url: 'https://test.salesforce.com/services/Soap/u/36.0',
 	    method: 'POST',
 	    headers: headers,
-	    data:'<?xml version="1.0" encoding="utf-8" ?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"><env:Body><n1:login xmlns:n1="urn:partner.soap.sforce.com"><n1:username>balink@capretraite.fr.dev3</n1:username><n1:password>1q2w3e4r5t</n1:password></n1:login></env:Body></env:Envelope>'
+	    body:'<?xml version="1.0" encoding="utf-8" ?><env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xmlns:env="http://schemas.xmlsoap.org/soap/envelope/"><env:Body><n1:login xmlns:n1="urn:partner.soap.sforce.com"><n1:username>balink@capretraite.fr.dev3</n1:username><n1:password>1q2w3e4r5t</n1:password></n1:login></env:Body></env:Envelope>'
 	}	
 	// Start the request
 	request(options, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
 	        // Print out the response body
-	       console.log(body);
+	       console.log('123'+body);
 	       var xml=body;
 	       parseString(xml, function (err, result) {
 			    var json=JSON.stringify(result);
